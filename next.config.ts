b900d1a047d4eccf33c6ffbd1bb86a3a528384a6
Davi S. Nascimento <nascimentoos.davi@gmail.com>
2025-06-05 23:41:58 -0300

    primeiro commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [
+    "http://localhost:3000",
+    "http://26.47.66.165:3000"
+  ],
+  devIndicators: false
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,102 +2,68 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <div className="header flex flex-col items-center justify-top p-4">
+        <div className="logo flex flex-row items-center justify-center text-center">
+          <Image
+            className="duck-logo "
+            src="/rubber-duck.png"
+            alt="duck"
+            width={33}
+            height={33}
+            priority
+          />
+          <h1 className="text-4xl font-extrabold m-2">Duck List</h1>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <p className="text-lg font-serif italic">&quot;A simple list for ducks&quot;</p>
+      </div>
+      <div className="flex flex-col items-center justify-center mt-4 mb-4">
+        <button
+          className="bg-yellow-800 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded-xl shadow-lg transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-yellow-700 focus:ring-opacity-50"
+          type="button">
+          <a href="#"
+            target="">Criar anotação</a>
+        </button>
+      </div>
+      <div className="list-container flex flex-col mx-10 sm:mx-40 md:mx-50 lg:mx-60 p-4 h-[65vh]  mb-4bg-blend-overlay  rounded-lg shadow-lg bg-gradient-to-br from-yellow-600 to-orange-950">
+
+        <div className="list flex flex-col justify-start w-full h-[33%] bg-neutral-800 rounded-lg mb-2">
+          <h2 className="texth2 font-bold  rounded-t-lg bg-gradient-to-r from-amber-900 to-yellow-400 from-10%  text-neutral-300 p-1">
+            To do
+          </h2>
+          <div className="flex flex-col items-center justify-center h-full">
+            <p className="text-neutral-400 font-thin text-sm">
+              Nenhuma anotação criada ainda.
+            </p>
+          </div>
+
+        </div>
+
+        <div className="list flex flex-col justify-start w-full h-[33%] bg-neutral-800 rounded-lg mb-2">
+          <h2 className="texth2 font-bold  rounded-t-lg bg-gradient-to-r from-amber-900 to-yellow-400 from-10% text-neutral-300 p-1">
+            Doing
+          </h2>
+          <div className="flex flex-col items-center justify-center h-full">
+            <p className="text-neutral-400 font-thin text-sm">
+              arraste uma anotação.
+            </p>
+          </div>
+        </div>
+
+        <div className="list flex flex-col justify-start w-full h-[33%] bg-neutral-800 rounded-lg mb-2">
+          <h2 className="texth2 font-bold  rounded-t-lg bg-gradient-to-r from-amber-900 to-yellow-400 from-10% text-neutral-300 p-1">
+            Done
+          </h2>
+          <div className="flex flex-col items-center justify-center h-full">
+            <p className="text-neutral-400 font-thin text-sm">
+              Nenhuma anotação criada ainda.
+            </p>
+          </div>
+        </div>
+
+      </div>
+
+
+    </>
   );
 }
